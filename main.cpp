0992164bee95f6508f149d59e754ef5f0ec43774
j-simon1 <42825336+j-simon1@users.noreply.github.com>
2018-10-21 13:39:31 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,14 +1,46 @@
-/**
-Description:
-Filename:
-Author:
-Date:
+*Description:This program will count how many times a charcter is present in a string.
+*Filename: main.cpp
+*Author: Joe Simon
+*Email: joes0243@student.vvc.edu
+*Date: 10-20-18
 */
+
+#include<iostream>
+#include<string>
 
 using namespace std;
 
 
+int get_string(int question)
+{
+        cin >> question;
+
+        return 0;
+}
+
+/**
+* find insstnace of a character
+*/
+
+ int char_count(int question, char p, int p_count)
+{
+        cin >> question;
+        int count = 0;
+        char t = 'p';
+        string q = "question";
+
+        p_count = t++;
+}
+
+
 int main()
 {
-   return 0;
+        cout << "Enter a phrase: " << endl;
+        int phs1;
+        cin >> phs1;
+
+        int question = get_string(phs1);
+        cout << "There are these many p's in your phrase: " << question << endl;
+
+        return 0;
 }
